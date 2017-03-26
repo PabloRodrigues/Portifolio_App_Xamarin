b834d950389c0be8035c4e643f2afe97508830c3
PabloRodrigues <pablorodrigues_rj@hotmail.com>
2017-03-26 16:15:53 -0300

    teste

---
+++ Cats/Cats/Cats/Services/AzureService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace Cats.Services
+{
+    /*
+    class AzureService
+    {
+    }
+    */
+
+    public class AzureService<T>
+    {
+        IMobileServiceClient Client;
+        IMobileServiceTable<T> Table;
+
+        //construtor
+        public AzureService()
+        {
+            string MyAppServiceURL = "http://CatasAppPablo.azurewebsites.net";
+            Client = new MobileServiceClient(MyAppServiceURL);
+            Table = Client.GetTable<T>();
+        }
+
+        //pegar dados da tabela IEnumerable
+        public Task<IEnumerable<T>> GetTable()
+        {
+            return Table.ToEnumerableAsync();
+        }
+    }
+}
---
+++ Cats/Cats/Cats/ViewModels/CatsViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Cats.Models;
+using Xamarin.Forms;
+
+namespace Cats.ViewModels
+{
+    public class CatsViewModel : INotifyPropertyChanged
+    {
+        //metodo construtor 
+        public CatsViewModel()
+        {
+            Cats = new ObservableCollection<Models.Cat>();
+
+            //Inicializa o GetCatsCommand
+            GetCatsCommand = new Command(async () => await GetCats(), () => !IsBusy);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(
+          [System.Runtime.CompilerServices.CallerMemberName]
+          string propertyName = null) =>
+          PropertyChanged?.Invoke(this,
+          new PropertyChangedEventArgs(propertyName));
+
+        private bool Busy;
+
+        public bool IsBusy
+        {
+            get
+            {
+                return Busy;
+            }
+            set
+            {
+                Busy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<Cat> Cats { get; set; }
+
+        
+
+        async Task GetCats()
+        {
+            if (!IsBusy)
+            {
+                Exception Error = null;
+                try
+                {
+                    IsBusy = true;
+                    var Repository = new Repository();
+                    var Items = await Repository.GetCats();
+
+                    //limpar a lista atual de objetos Cat e carregá - los a partir da coleção Items.
+                    Cats.Clear();
+                    foreach (var Cat in Items)
+                    {
+                        Cats.Add(Cat);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Error = ex;
+                }
+                finally
+                {
+                    IsBusy = false;
+                }
+
+                //Mensagem de erro.
+                if (Error != null)
+                {
+                    await Xamarin.Forms.Application.Current.MainPage.DisplayAlert(
+                    "Error!", Error.Message, "OK");
+                }
+
+                            }
+            return; 
+        }
+
+        public Command GetCatsCommand { get; set; }
+
+
+    }
+}
